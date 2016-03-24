0ab23b4b225ca594bc9e140685e2504f547fd38e
Justin <Rossj190@gmail.com>
2016-03-23 21:52:14 -0700

    GET

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080, function(){
-    console.log('Server running on 8080...');
+var express = require('express');
+ 
+var app = express();
+app.use(express.static(__dirname));
+ 
+var port = 8080;
+app.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
+
+app.get('/fff', function (req, res, next) {
+  res.send('asdfS');
 });
