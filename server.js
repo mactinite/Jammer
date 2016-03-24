2f6c52c083971a04c3932576cfa13bd84bb5305e
Justin Ross <Rossj190@gmail.com>
2016-03-24 15:47:44 -0700

    Event GET

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
+var fs = require('fs');
+
 
 
 app.use(favicon(__dirname + '/images/favicon.ico'));
 app.use(express.static(__dirname));
- 
+
 var port = 8080;
 app.listen(port, function() {
     console.log('server listening on port ' + port);
 });
 
-app.get('/fff', function (req, res, next) {
-  res.send('asdfS');
+
+app.get('/Event', function(req, res, next) {
+    //var id = req.body.id;
+    var content = fs.readFileSync("Data/events.json");
+    var json = JSON.parse(content);
+    res.set('Content-Type','application/json')
+    res.send(json);
 });
