8361f93c4d58c7f88e15df7130ca8a994a346fdf
MACDONALD <hm476j@att.com>
2016-03-24 15:29:07 -0700

    Added bootstrap and angular get call

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,10 @@
 var eventCalendar = angular.module('eventCalendar', []);
 
-eventCalendar.controller('EventController', function ($scope) {
-  $scope.events = [
-    {'name': 'Camping',
-     'description': 'Grow a beard with this patented technique! Hunter is going!'},
-    {'name': 'Sakura Con™',
-     'description': 'Get your weeb on! Justin might go!'},
-    {'name': 'Mariner\'s game',
-     'description': 'Don\'t buy drinks at the game! Pre-game! Saravpreet is going!'}
-  ];
+eventCalendar.controller('EventController', function ($scope,$http) {
+    $http.get("data/events.json").then(function(response){
+       $scope.data = response.data; 
+    },
+    function(error){
+        
+    });
 });
