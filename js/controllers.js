9421760ed1663dac4cf6d96de465d088b7b68d79
MACDONALD <mactinite77@gmail.com>
2016-03-29 17:02:52 -0700

    Added ordinal conversion and made the calendar a horizontal scrollview.
    Started working on adding events to certain days.

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,17 @@
 var eventCalendar = angular.module('eventCalendar', []);
-
+var events = [];
 eventCalendar.controller('EventController', function($scope, $http) {
     $http.get("data/events.json").then(function(response) {
         $scope.data = response.data;
+        events = $scope.data.events;
+        
     },
         function(error) {
 
         });
 });
+
+
 
 /*Calendar Builder
   Here is where the Calendar will be built based on todays date. 
@@ -42,34 +46,27 @@ Calendar.prototype.generateHTML = function() {
 
     var monthName = cal_months_labels[this.month];
     var html = '<div class="calendar-wrapper">';
-    html += '<h2 class="center">';
+    html += '<h2 class="center calendar-month">';
     html += monthName + "&nbsp;" + this.year;
     html += '</h2>';
-    html += '<ul class="calendar-daysOfWeek">';
-    for (var i = 0; i <= 6; i++) {
-        html += '<li class="WeekDay">';
-        html += cal_days_labels[i];
-        html += '</li>';
-    }
     html += '</ul><ul class="calendar-week">';
 
     var day = 1;
-    // this loop is for is weeks (rows)
-    for (var i = 0; i < 9; i++) {
-        // this loop is for weekdays (cells)
-        for (var j = 0; j <= 6; j++) {
-            html += '<li class="day">';
-            if (day <= monthLength && (i > 0 || j >= startingDay)) {
-                html += day;
-                day++;
-            }
-            html += '</li>';
-        }
+    // this loop is for is days (rows)
+    for (var i = 0; i < monthLength; i++) {
+        html += '<li>';
+        html += '<span>';
+        html += monthName + " " + getOrdinal(day);
+        html += '</span>';
+        var toMatch = this.year + "-" + this.month + "-" + day;
+        
+        day++;
+        html += '</li>';
         // stop making rows if we've run out of days
         if (day > monthLength) {
             break;
         } else {
-            html += '</ul><ul class="calendar-week">';
+            html += '';
         }
     }
 
@@ -80,9 +77,15 @@ Calendar.prototype.generateHTML = function() {
 }
 
 Calendar.prototype.getHTML = function() {
-  return this.html;
+    return this.html;
 }
 
+
+function getOrdinal(n) {
+    var s = ['th', 'st', 'nd', 'rd'];
+    var v = n % 100;
+    return n + (s[n % 10] || s[v] || s[0]);
+}
 
 eventCalendar.controller("CalendarController", function($scope) {
 
