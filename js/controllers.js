b44003e204226ad24c98fa9004adf825b4b0c36e
Hunter MacDonald <Hunter MacDonald>
2016-04-20 22:31:05 -0700

    Set up Jam and Classified listings + styling

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,12 @@
-var eventCalendar = angular.module('eventCalendar', []);
-var events = [];
-eventCalendar.controller('EventController', function($scope, $http) {
-    $http.get("data/events.json").then(function(response) {
-        $scope.data = response.data;
-        events = $scope.data.events;
-        
-    },
-        function(error) {
+var jammer = angular.module('jammer', []);
 
-        });
+
+jammer.controller('postingArea', function($scope, $http) {
+    
+    $scope.getNumber = function(num){
+        return new Array(num);
+    }
+    
+    $scope.jobTypes = ["Programmers", "Artists", "Musicians", "Other"];
+        
 });
