9afb405fbbe0bc38aa19c283b50ed8ea31163c1b
ROSS <jr476j@att.com>
2016-04-22 17:15:55 -0700

    Database and routing stuff

---
+++ app/routes/posts.js
@@ -0,0 +1,36 @@
+module.exports = (function () {
+    var router = require('express').Router();
+    var r = require('rethinkdb');
+    var db = require('../../config/database');
+    var p = r.connect({db: 'Jammer'});
+
+    //Get jam posts with optional time range parameter
+    router.get('/jamPosts', function (req, res, next) {
+        p.then(function(conn)){
+            //TODO: fix this.
+        }.error(function(error)){
+            throw error;
+        };
+        r.connect(db.db, function (err, conn) {
+            if (err) throw err;
+            if (req.body.date !== undefined) {
+                //TODO: Get filter by date working
+                r.table(db.table.jam)
+                    .filter((r.row["startDate"] - new Date().getDate()) > 0)
+                    .run(conn, function (err, result) {
+                        if (err) throw err;
+                        res.send(JSON.stringify(result, null, 2));
+                    });
+            }
+            else {
+                r.table(db.table.jam).get('asdf')
+                    .run(conn, function (err, result) {
+                        if (err) throw err;
+                        res.send(JSON.stringify(result, null, 2));
+                    });
+            }
+        });
+    });
+
+    return router;
+})();
